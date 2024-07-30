4a1a9bfacbc0cc3188e791c52aeb1f0a77c98e45
Muzi <mmzolo@dearx.co.za>
2024-07-30 16:11:36 +0200

    Add empty squares

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-export default function Square() {
+export default function Board() {
   return (
   <>
   <div className="board-row">
-  <button className="square">1</button>
-  <button className="square">2</button>
-  <button className="square">3</button>
-  </div>
+    <Square/>
+    <Square/>
+    <Square/>
+    </div>
   <div className="board-row">
-  <button className="square">4</button>
-  <button className="square">5</button>
-  <button className="square">6</button>
-  </div>
+    <Square/>
+    <Square/>
+    <Square/>
+    </div>
   <div className="board-row">
-  <button className="square">7</button>
-  <button className="square">8</button>
-  <button className="square">9</button>
+    <Square/>
+    <Square/>
+    <Square/>
   </div>
   </>
   );
+}
+
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
